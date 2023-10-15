fb976bb3e5994c3ea0b21d1215b853e8017439e1
hamed-Cloner <najafi_hamed@live.com>
2023-10-15 12:16:14 +0330

    2nd Commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
         List<Personel> people = new ArrayList<Personel>();
-
+        for(int i = 0; i < 50; i++){
+            people.add(new Personel());
+        }
         System.out.println(people);
 
 

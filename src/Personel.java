71da5864a1196a52a12c89081b339fdc13365bdf
hamed-Cloner <najafi_hamed@live.com>
2023-10-15 13:45:19 +0330

    2nd Commit

--- src/Personel.java
+++ src/Personel.java
@@ -1,21 +1,27 @@
 import java.util.Random;
 
 public class Personel{
+
+
     private String name;
     private int age;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     private String id;
 
      Random random = new Random();
     public Personel() {
-        this.name = PersonUtils.nameGenerator(5);
-        this.age = random.nextInt(98) + 1;
-        this.id = PersonUtils.UniqueIdGenerator("[0-9]", 10);
+        this.name = name;
+        this.age = age;
+        this.id = id;
 
+    }
+    public void setAge(int age) {
+        this.age = random.nextInt(98) + 1;
+    }
+    public void setName(String name) {
+        this.name = PersonUtils.nameGenerator(5);
+    }
+    public void setId(String id) {
+        this.id = PersonUtils.UniqueIdGenerator("[0-9]", 10);
     }
     public String getName() {
         return name;
